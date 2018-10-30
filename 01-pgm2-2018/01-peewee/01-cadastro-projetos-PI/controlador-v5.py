0f0cd2893fa8c5f1b642f8f06b693990a6c8fe89
Hylson <hvescovi@gmail.com>
2018-10-30 11:05:44 -0300

    recurso de exclusão de projetos

---
+++ 01-pgm2-2018/01-peewee/01-cadastro-projetos-PI/controlador-v5.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from modelo import *
+
+app =  Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "Sistema de cadastro de projetos "+\
+    "integradores. <a href=/listar>"+\
+    "Clique aqui</a> para começar."
+
+@app.route("/listar")
+def listar():
+    trabs = TrabalhoPI.select()
+    return render_template("listar-v4.html", lista = trabs)
+
+@app.route("/form_inserir")
+def form_inserir():
+    alunos = Aluno.select()
+    docentes = Docente.select()
+    return render_template("form_incluir_v1.html", 
+        alunos=alunos, docentes=docentes)
+
+@app.route("/inserir",methods=["post"])
+def inserir():
+    aluno = Aluno.get(id = request.form['aluno'])
+    prof = Docente.get(id = request.form['docente'])
+
+    t1 = TrabalhoPI.create(titulo = request.form['titulo'],
+        descricao = request.form['descricao'],
+        url = request.form['url'])
+    t1.alunos.add(aluno)
+    t1.docentes.add(prof)
+    return listar()
+
+@app.route("/excluir")
+def excluir():
+    id_pi = request.args.get("id")
+    TrabalhoPI.delete_by_id(id_pi)
+    return listar()
+
+app.run(debug=True)
